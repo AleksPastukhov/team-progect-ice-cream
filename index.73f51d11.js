e7c1f15b2cba5dbeab27ab1cdb4bf7fa421a5028
AleksPastukhov <109807722+AleksPastukhov@users.noreply.github.com>
2023-01-14 18:18:57 +0000

    Deploying to gh-pages from @ AleksPastukhov/team-project-ice-cream@3084f2fb1d73b733de78a9f37de37cb22fb585bd 🚀

---
+++ index.73f51d11.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-modal-map-open]"),closeModalBtn:document.querySelector("[data-modal-map-close]"),modal:document.querySelector("[data-modal-map]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.73f51d11.js.map
---
+++ index.b6abbcc7.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-modal-fran-open]"),closeModalBtn:document.querySelector("[data-modal-fran-close]"),modal:document.querySelector("[data-modal-fran]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.b6abbcc7.js.map
